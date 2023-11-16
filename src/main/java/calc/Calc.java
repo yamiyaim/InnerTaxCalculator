b35b7a45ddf437c9f8394557fffe96c94ddc52cf
yamiyaim <gmlals77720@gmail.com>
2023-11-16 15:40:22 +0900

    텍스트 변경

--- src/main/java/calc/Calc.java
+++ src/main/java/calc/Calc.java
@@ -2,7 +2,7 @@ package calc;
 
 public class Calc {
     public static int[] calculateTaxAndCost(int price) {
-        int[] answer = { price, price }; // 변경해야 하는 부분
+        int[] answer = { price, price }; // 변경해야 하는 부분 0번 인덱스가 cost(원가), 1번 인덱스가 tax(소비세)가 되도록 작성바랍니다.
         return answer;
     }
 }
